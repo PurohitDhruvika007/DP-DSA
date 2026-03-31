7c1d02a0f1889a2966244be74040c61d59821363
dhruvika <purohitdhruvika87@gmail.com>
2026-03-31 09:18:27 +0530

    31-03-2026

--- Queue/linkedList.js
+++ Queue/linkedList.js
@@ -9,13 +9,49 @@ class Queue {
         this.front = null;
         this.rear = null;
     }
-    insert(value) {
+    enqueue(value) {
         let newNode = new Node(value);
         if (!this.rear) {
-
+            this.front = this.rear = newNode;
+            return;
         }
+        let current = this.front
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+        this.rear = newNode;
+    }
+    print() {
+        let current = this.front;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+    dequeue() {
+        if (!this.front) {
+            console.log('queue is underflow');
+            return;
+        }
+        this.front = this.front.next;
+    }
+    isEmpty() {
+        if (!this.front) {
+            console.log('queue is empty.');
+            return;
+        }
+        console.log('queue is not empty.');
     }
 }
 
 let q = new Queue();
-q.insert(10);
+q.isEmpty();
+q.enqueue(10);
+q.enqueue(20);
+q.enqueue(30);
+q.enqueue(40);
+q.dequeue();
+q.dequeue();
+q.print();
+q.isEmpty();
