f607be69249487f906bb33a4319a630bd84ef512
dhruvika <purohitdhruvika87@gmail.com>
2026-04-03 20:40:39 +0530

    03-04-2026

--- Recursion/main.js
+++ Recursion/main.js
@@ -1,5 +1,50 @@
 class Recursion {
-
+    printNtoN(start, end) {
+        console.log(start);
+        if (start < end) {
+            start++;
+            this.printNtoN(start, end);
+        }
+        if (start > end) {
+            start--;
+            this.printNtoN(start, end);
+        }
+    }
+    fibonacci(first, second, n) {
+        if (first == 0) {
+            console.log(first);
+            console.log(second);
+        }
+        let ans = first + second;
+        first = second;
+        second = ans;
+        console.log(second);
+        if (n > 0) {
+            n--;
+            this.fibonacci(first, second, n);
+        }
+    }
+    one(n, start) {
+        console.log(start)
+        if (n > 0) {
+            n--;
+            this.one(n, start / 2);
+        }
+    }
+    two(n, first, second) {
+        console.log(first);
+        console.log(second);
+        if (n > 0) {
+            n--;
+            this.two(n, first + 1, second + 1);
+        }
+    }
 }
 
 const math = new Recursion();
+// math.printNtoN(3, 8);
+// math.printNtoN(9, 2);
+// math.fibonacci(0, 1, 6);
+// math.one(6, 8);
+math.two(4, 7, 10);
+
