352494f7d7e1fc78e4a46213b382647a547fd7cf
dhruvika <purohitdhruvika87@gmail.com>
2026-04-07 10:49:37 +0530

    07-04-2026

--- BinaryTree/main.js
+++ BinaryTree/main.js
@@ -39,6 +39,44 @@ class BinaryTree {
             }
         }
     }
+    search(value) {
+        let current = this.root;
+        while (current) {
+            if (current.data == value) {
+                return true;
+            }
+            if (value < current.data) {
+                current = current.left;
+            }
+            else {
+                current = current.right;
+            }
+        }
+        if (current == null) {
+            return false;
+        }
+    }
+    inorder(node) {
+        if (node != null) {
+            this.inorder(node.left);
+            console.log(node.data);
+            this.inorder(node.right);
+        }
+    }
+    preorder(node) {
+        if (node !== null) {
+            console.log(node.data);
+            this.preorder(node.left);
+            this.preorder(node.right);
+        }
+    }
+    postorder(node) {
+        if (node !== null) {
+            this.postorder(node.left);
+            this.postorder(node.right);
+            console.log(node.data);
+        }
+    }
 }
 
 const bt = new BinaryTree();
@@ -47,9 +85,24 @@ bt.insert(10);
 bt.insert(1);
 bt.insert(55);
 bt.insert(45);
+bt.insert(33);
+bt.insert(60);
+bt.insert(78);
+bt.insert(65);
+bt.insert(12);
 
-console.log(bt.root.data);
-console.log(bt.root.left.data);
-console.log(bt.root.left.left.data);
-console.log(bt.root.right.data);
-console.log(bt.root.right.left.data);
+
+let res = bt.search(45);
+console.log(res);
+
+
+// bt.inorder(bt.root);
+// bt.preorder(bt.root);
+// bt.postorder(bt.root);
+
+
+// console.log(bt.root.data);
+// console.log(bt.root.left.data);
+// console.log(bt.root.left.left.data);
+// console.log(bt.root.right.data);
+// console.log(bt.root.right.left.data);
